677db49d5b621382d6b479f841a76a4da0c8c679
Patrik Wehovszky <wpatrik53@gmail.com>
2022-10-31 20:11:01 +0100

    added: ThymeleafWebConfig.java for templateResolver and templateEngine and login page ViewController to registry

---
+++ src/main/java/hu/mik/prog5/z7twzr/mediamark/config/ThymeleafWebConfig.java
@@ -0,0 +1,40 @@
+package hu.mik.prog5.z7twzr.mediamark.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafWebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(SpringSecurityDialect springSecurityDialect) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.setTemplateResolver(this.templateResolver());
+        templateEngine.addDialect(springSecurityDialect);
+
+        return templateEngine;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
