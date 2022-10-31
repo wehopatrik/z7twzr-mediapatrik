721f36a67ae2ec4a04adae43b3406f645a7cfe6d
Patrik Wehovszky <wpatrik53@gmail.com>
2022-10-31 15:43:14 +0100

    feat: initializing the project from 'start.spring.io' with the expected dependencies

---
+++ src/main/java/hu/mik/prog5/z7twzr/mediamark/MediamarkApplication.java
@@ -0,0 +1,13 @@
+package hu.mik.prog5.z7twzr.mediamark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediamarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediamarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/mik/prog5/z7twzr/mediamark/MediamarkApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.mik.prog5.z7twzr.mediamark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediamarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
