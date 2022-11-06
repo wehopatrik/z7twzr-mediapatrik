7383147c0214907eb49ffc0bc9005c1485db7333
Patrik Wehovszky <wpatrik53@gmail.com>
2022-11-06 15:39:56 +0100

    modified: IndexController for root

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/IndexController.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("")
     public String getRoot() {
         return "redirect:index";
     }
