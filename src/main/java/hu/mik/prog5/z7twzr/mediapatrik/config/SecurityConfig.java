810b6dd7dabe09fe26929c3ad8493306b5c62126
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-09 13:27:46 +0100

    modified: SecurityConfig class and common.html for registration URL whitelist and a tag

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/config/SecurityConfig.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private static final String[] AUTH_WHITELIST = { "/", "/index", "/home/**", "/shop/**", "/product/**", "/css/**", "/js/**", "/images/**", "/favicon.ico**" };
+    private static final String[] AUTH_WHITELIST = { "/", "/index", "/register/**", "/home/**", "/shop/**", "/product/**", "/css/**", "/js/**", "/images/**", "/favicon.ico**" };
 
     @Bean
     public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
