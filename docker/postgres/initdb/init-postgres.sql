34ce5e0244c8eaac19178858c8ea3d0833811fb5
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-09 17:49:43 +0100

    modified: admin.html

--- docker/postgres/initdb/init-postgres.sql
+++ docker/postgres/initdb/init-postgres.sql
@@ -50,6 +50,10 @@ INSERT INTO "user" (username, password) VALUES ('user', '$2a$12$ceJaZ2NVdYgAfCyS
 INSERT INTO role (role_name) VALUES ('USER');
 INSERT INTO role (role_name) VALUES ('ADMIN');
 
+INSERT INTO product (name, image_name, price, type) VALUES ('iPhone 13 Pro Max', 'iphone13promax', 359990, 'MOBILE');
+INSERT INTO product (name, image_name, price, type) VALUES ('iPad 10. generáció', 'ipad10', 259990, 'TABLET');
+INSERT INTO product (name, image_name, price, type) VALUES ('Macbook Pro M2', 'macbookprom2', 529990, 'LAPTOP');
+
 INSERT INTO user_role (user_id, role_id) SELECT * FROM (SELECT u.id FROM "user" u WHERE u.username = 'admin') a, (SELECT r.id FROM "role" r WHERE r.role_name = 'ADMIN') b;
 INSERT INTO user_role (user_id, role_id) SELECT * FROM (SELECT u.id FROM "user" u WHERE u.username = 'admin') a, (SELECT r.id FROM "role" r WHERE r.role_name = 'USER') b;
 INSERT INTO user_role (user_id, role_id) SELECT * FROM (SELECT u.id FROM "user" u WHERE u.username = 'user') a, (SELECT r.id FROM "role" r WHERE r.role_name = 'USER') b;
