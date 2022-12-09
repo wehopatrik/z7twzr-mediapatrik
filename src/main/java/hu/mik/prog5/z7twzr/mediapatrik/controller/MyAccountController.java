e616d7cfa69c31dc3d4df92873e52393661ff197
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-09 12:25:17 +0100

    added: MyAccountController class and myaccount.html file for my account page

---
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/MyAccountController.java
@@ -0,0 +1,14 @@
+package hu.mik.prog5.z7twzr.mediapatrik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyAccountController {
+
+    @GetMapping("my-account")
+    public String getHomePage() {
+        return "myaccount";
+    }
+
+}
