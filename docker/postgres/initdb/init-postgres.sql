63eb74a729098ea033d1376a4c5198d9f7a08b72
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-09 11:06:03 +0100

    added: init-postgres.sql and docker-compose.yml for Docker PostgreSQL DB

---
+++ docker/postgres/initdb/init-postgres.sql
@@ -0,0 +1,48 @@
+CREATE ROLE mediapatrik WITH
+    LOGIN
+    SUPERUSER
+    CREATEDB
+    CREATEROLE
+    INHERIT
+    REPLICATION
+    CONNECTION LIMIT -1
+    PASSWORD 'Password111';
+
+CREATE TABLE product
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
+    image_name  VARCHAR(100) NOT NULL,
+    price       INT NOT NULL,
+    type        VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE "user"
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE role
+(
+    id        SERIAL PRIMARY KEY,
+    role_name VARCHAR(100)
+);
+
+CREATE TABLE user_role
+(
+    id      SERIAL PRIMARY KEY,
+    user_id SERIAL NOT NULL REFERENCES "user" (id),
+    role_id SERIAL NOT NULL REFERENCES role (id)
+);
+
+INSERT INTO "user" (username, password) VALUES ('admin', '$2a$12$ceJaZ2NVdYgAfCySr55xTu5y1SWpovxAIwLv3AlLvuwxemCb1Uple');
+INSERT INTO "user" (username, password) VALUES ('user', '$2a$12$ceJaZ2NVdYgAfCySr55xTu5y1SWpovxAIwLv3AlLvuwxemCb1Uple');
+
+INSERT INTO role (role_name) VALUES ('USER');
+INSERT INTO role (role_name) VALUES ('ADMIN');
+
+INSERT INTO user_role (user_id, role_id) SELECT * FROM (SELECT u.id FROM "user" u WHERE u.username = 'admin') a, (SELECT r.id FROM "role" r WHERE r.role_name = 'ADMIN') b;
+INSERT INTO user_role (user_id, role_id) SELECT * FROM (SELECT u.id FROM "user" u WHERE u.username = 'admin') a, (SELECT r.id FROM "role" r WHERE r.role_name = 'USER') b;
+INSERT INTO user_role (user_id, role_id) SELECT * FROM (SELECT u.id FROM "user" u WHERE u.username = 'user') a, (SELECT r.id FROM "role" r WHERE r.role_name = 'USER') b;
