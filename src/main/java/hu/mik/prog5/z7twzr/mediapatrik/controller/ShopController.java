75d646faa78a60c510b4e15d4b081422572c7b35
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-09 18:32:31 +0100

    modified: ShopController class for modelAttribute to list all products

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/ShopController.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/ShopController.java
@@ -1,13 +1,20 @@
 package hu.mik.prog5.z7twzr.mediapatrik.controller;
 
+import hu.mik.prog5.z7twzr.mediapatrik.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ShopController {
 
+    private final ProductService productService;
+
     @GetMapping("shop")
-    public String getShopPage() {
+    public String getShopPage(Model model) {
+        model.addAttribute("products", productService.findAll());
         return "shop";
     }
 
