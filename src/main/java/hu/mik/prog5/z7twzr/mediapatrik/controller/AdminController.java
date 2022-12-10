a0f3496f484b04b742538c1ffb1163fe8479ce37
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-10 13:56:09 +0100

    modified: AdminController class and admin.html page for listing all orders

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/AdminController.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/AdminController.java
@@ -1,13 +1,20 @@
 package hu.mik.prog5.z7twzr.mediapatrik.controller;
 
+import hu.mik.prog5.z7twzr.mediapatrik.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final ProductService productService;
+
     @GetMapping("admin")
-    public String getAdminPage() {
+    public String getAdminPage(Model model) {
+        model.addAttribute("orderedProducts", productService.findAllOrders());
         return "admin";
     }
 
