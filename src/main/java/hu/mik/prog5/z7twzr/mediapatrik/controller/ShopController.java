97be599cb34109acea56543fdb9fa57834d9ef51
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-10 13:26:54 +0100

    modified: ShopController class for implementing the shopping function

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/ShopController.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/ShopController.java
@@ -1,9 +1,12 @@
 package hu.mik.prog5.z7twzr.mediapatrik.controller;
 
 import hu.mik.prog5.z7twzr.mediapatrik.entity.Product;
+import hu.mik.prog5.z7twzr.mediapatrik.entity.User;
 import hu.mik.prog5.z7twzr.mediapatrik.service.ProductService;
+import hu.mik.prog5.z7twzr.mediapatrik.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ShopController {
 
     private final ProductService productService;
+    private final UserService userService;
 
     @GetMapping("shop")
     public String getShopPage(Model model) {
@@ -28,7 +32,19 @@ public class ShopController {
     @ResponseBody
     public String shop(@RequestBody Product product, Model model) {
         try {
+            // Termék kikérése
             Product foundProductInDb = productService.findById(product.getId());
+
+            // Felhasználó kikérése
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            User user = userService.findByUsername(username);
+
+            // Rendelés
+            boolean isSuccessful = productService.order(foundProductInDb.getId(), user.getId());
+            if(!isSuccessful) {
+                return "Error";
+            }
+
             log.info("PRODUCT ORDERED: " + foundProductInDb.toString());
             return "Done";
         } catch(Exception e) {
