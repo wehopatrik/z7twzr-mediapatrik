1e3e723a98a4683e2a338179c59d701d0d579eba
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-10 13:27:15 +0100

    modified: MyAccountController class for listing all ordered products by user

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/MyAccountController.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/MyAccountController.java
@@ -1,13 +1,27 @@
 package hu.mik.prog5.z7twzr.mediapatrik.controller;
 
+import hu.mik.prog5.z7twzr.mediapatrik.entity.User;
+import hu.mik.prog5.z7twzr.mediapatrik.service.ProductService;
+import hu.mik.prog5.z7twzr.mediapatrik.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MyAccountController {
 
+    private final ProductService productService;
+    private final UserService userService;
+
     @GetMapping("my-account")
-    public String getMyAccountPage() {
+    public String getMyAccountPage(Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.findByUsername(username);
+
+        model.addAttribute("orderedProducts", productService.findAllOrders(user.getId()));
         return "myaccount";
     }
 
