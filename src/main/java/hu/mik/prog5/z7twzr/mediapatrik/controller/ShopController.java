804bb42034d1e5de2ae2e04bec037e36091d9671
Patrik Wehovszky <wpatrik53@gmail.com>
2022-12-10 11:32:01 +0100

    modified: ShopController for /shop post method for shopping

--- src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/ShopController.java
+++ src/main/java/hu/mik/prog5/z7twzr/mediapatrik/controller/ShopController.java
@@ -1,11 +1,17 @@
 package hu.mik.prog5.z7twzr.mediapatrik.controller;
 
+import hu.mik.prog5.z7twzr.mediapatrik.entity.Product;
 import hu.mik.prog5.z7twzr.mediapatrik.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ShopController {
@@ -16,6 +22,19 @@ public class ShopController {
     public String getShopPage(Model model) {
         model.addAttribute("products", productService.findAll());
         return "shop";
+    }
+
+    @PostMapping("shop")
+    @ResponseBody
+    public String shop(@RequestBody Product product, Model model) {
+        try {
+            productService.findById(product.getId());
+            log.info("PRODUCT ORDERED: " + product.toString());
+            return "Done";
+        } catch(Exception e) {
+            log.error("ERROR HAPPENED WHEN ORDERING PRODUCT: " + product.toString() + " | ERROR: " + e);
+            return "Error";
+        }
     }
 
 }
